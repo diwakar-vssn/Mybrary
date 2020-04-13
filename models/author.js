e49db42dd97fee890703974c9bcae34f03357080
diwakar-vssn <diwakar.vssn@gmail.com>
2020-04-13 12:57:16 +0530

    Adding View/Edit/Delete routes for author

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongo = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongo.Schema({
     name:{
@@ -6,6 +7,18 @@ const authorSchema = new mongo.Schema({
         required:true
     }
 
+})
+
+authorSchema.pre('remove', function(next){
+    Book.find({author:this.id}, (err, books)=>{
+        if(err){
+            next(err)
+        }else if(books.length>0){
+            next(new Error("This author still has books"))
+        }else{
+            next()
+        }
+    })
 })
 
 module.exports = mongo.model('Author' , authorSchema)
--- routes/authors.js
+++ routes/authors.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.name!=null || req.query.name!=''){
-        searchOptions.name = RegExp(req.query.name, 'i') 
+    if (req.query.name != null || req.query.name != '') {
+        searchOptions.name = RegExp(req.query.name, 'i')
     }
     try {
         const authors = await Author.find(searchOptions)
-        res.render('authors/index',{
-            authors:authors,
-            searchOptions : req.query
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
         })
     }
     catch{
@@ -36,10 +37,72 @@ router.post('/', (req, rsp) => {
             })
         }
         else {
-            rsp.redirect('authors')
+            rsp.redirect(`authors/${newAuthor.id}`)
         }
     })
 
+})
+
+router.get('/:id', async (req, rsp) => {
+  try{
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({author:author.id}).limit(10).exec()
+    rsp.render('authors/show',{
+        author: author,
+        booksByAuthor: books
+    })
+  }
+  catch{
+    rsp.redirect('authors')
+  }
+})
+
+router.get('/:id/edit', async (req, rsp) => {
+
+    try {
+        const author = await Author.findById(req.params.id);
+        rsp.render('authors/edit', { author: author })
+    }
+    catch{
+        rsp.redirect('/authors')
+    }
+
+})
+
+router.put('/:id', async (req, rsp) => {
+    let author
+    try{
+        author = await Author.findById(req.params.id)  
+        author.name = req.body.authorName
+        await author.save()
+        rsp.redirect(`/authors/${req.params.id}`)
+    }
+    catch{
+        if(author == null){
+            rsp.redirect('/')
+        }else{
+            rsp.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating author'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, rsp) => {
+    let author
+    try{
+        author = await Author.findById(req.params.id)  
+        await author.remove()
+        rsp.redirect('/authors')
+    }
+    catch(err){
+        if(author == null){
+            rsp.redirect('/')
+        }else{
+            rsp.redirect(`/authors/${author.id}`)
+        }
+    }
 })
 
 module.exports = router
